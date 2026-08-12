d2e41388adada67cddb207be3e2a1188cffcd245
yvic82501-arch <yvic.82501@gmail.com>
2026-08-13 00:55:29 +0530

    Update Venue.jsx

--- Cafe-client/src/pages/Venue.jsx
+++ Cafe-client/src/pages/Venue.jsx
@@ -44,7 +44,7 @@ function Venue() {
                 </span>
               </p>
               <a
-                href={`/queue/${queue._id}`}
+                href={`/queue/${queueId}`}
                 className="inline-block mt-4 bg-green-600 text-white px-4 py-2 rounded-lg hover:shadow-lg"
               >
                 View Queue
