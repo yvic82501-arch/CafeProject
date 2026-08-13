c6554930cfb8c91a1c666ca11cad746b64769c28
yvic82501-arch <yvic.82501@gmail.com>
2026-08-13 17:46:38 +0530

    Update ProtectedRoute.jsx

--- Cafe-client/src/components/ProtectedRoute.jsx
+++ Cafe-client/src/components/ProtectedRoute.jsx
@@ -1,16 +1,13 @@
-import { useNavigate } from "react-router-dom";
-
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user"));
-  const navigate = useNavigate();
 
   if (!token || !user) {
-    return <navigate to="/login" />;
+    return <Navigate to="/login" />;
   }
 
   if (user.role !== "admin" && user.role !== "superadmin") {
-    return <navigate to="/" />;
+    return <Navigate to="/" />;
   }
 
   return children;
